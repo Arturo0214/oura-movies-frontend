e9a4c3443fa363980c7b616d9b73a21b1192e22a
Arturo0214 <osvaldosuarezcruz@gmail.com>
2023-05-12 12:02:17 -0600

    added react-player

--- src/components/MovieModal.jsx
+++ src/components/MovieModal.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import Modal from 'react-modal';
+import React from 'react'
+import Modal from 'react-modal'
+import ReactPlayer from 'react-player'
 
 const customStyles = {
   content: {
-    top: 'calc(50% - 170px)',
-    left: 'calc(50% - 400px)',
+    top: 'calc(50% - 240px)',
+    left: 'calc(50% - 490px)',
     right: 'auto',
     bottom: 'auto',
     marginRight: 'auto',
     transform: 'rotateX(20deg)',
-    maxWidth: '850px',
+    maxWidth: '1050px',
     border: '1px solid #ccc',
     borderRadius: '20px',
     boxShadow: "2px 2px 10px rgba(0, 0, 0, 0.2)",
@@ -26,14 +27,22 @@ const MovieModal = ({ isOpen, onRequestClose, movie}) => {
       style={customStyles}
     >
       <h2 className='modal-title'>{movie.title}</h2>
-      <div className="row">
-        <div className="col-6">
-          <img src={movie.link} alt={movie.title} className='modal-image' />
+      <div className="trailer">
+        <div className="trailer" style={{width: '70%', height: '70%'}}>
+          <ReactPlayer
+            className='player'
+            url={movie.trailer}
+            width='450px'
+            height='300px'
+            controls
+            playing
+            volume='0.8'
+          />
         </div>
         <div className="col-6">
+          <label className='font-title'><strong>Overview</strong></label>
           <p className='font'>{movie.overview}</p>
-          <label>Release Date</label>
-          <p className='font'> {movie.release_date} </p>
+
         </div>
       </div>
       <button onClick={onRequestClose} className="btn btn-danger close-2">Close</button>
